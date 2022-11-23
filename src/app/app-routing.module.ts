624f865e538f24ed9b83021f191a4092569e77ca
BELLAHA Sarah (SNCF VOYAGEURS / Etablissement Gares Transilien Sud Ligne C / TERR JUINE ET ORGE) <9008080s@commun.ad.sncf.fr>
2022-11-23 14:00:09 +0100

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ToysComponent } from './toys/toys.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "toys", component: ToysComponent},
+  {path: "toys", component: ToysComponent},
+  {path: "useraccount", component: UserprofileComponent},
+  {path: "signup", component: SignupComponent},
+  {path: "login", component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
