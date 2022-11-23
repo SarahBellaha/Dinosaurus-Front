0c269ad9bb8900fadcd3e16824a43cfdc8fb4e65
loicWCS <loic.lebon.wcs@gmail.com>
2022-11-24 00:19:28 +0100

    home and login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../classes/user';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  @Input()
+  user: User = new User();
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  gotoHome() {
+    this.router.navigateByUrl('/home');
   }
+  gotoAccount() {
+    this.router.navigateByUrl('/useraccount');
+  }
+  onSubmit() {
+    this.user.password == this.user.password
+      ? this.gotoAccount()
+      : this.gotoHome();
+  }
+  // Add user to DB
 
+  ngOnInit(): void {}
 }
