b1655715ab961904e1bfc1f3541759604956646f
loicWCS <loic.lebon.wcs@gmail.com>
2025-07-17 10:19:59 +0200

    rename url

--- src/app/Toys-Service/toys-service.service.ts
+++ src/app/Toys-Service/toys-service.service.ts
@@ -1,45 +1,39 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Toy } from '../Interfaces/Toy';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ToysServiceService {
+  private readonly http: HttpClient = inject(HttpClient);
 
-  constructor(private http: HttpClient) {}
+  private readonly toyUrl = 'http://localhost:8080/toys';
+  private readonly usersUrl = 'http://localhost:8080/users';
 
-  private dinoUrl = "http://localhost:8080/toys";
-  private usersUrl = "http://localhost:8080/users";
-
-  getToys(): Observable<Toy[]>{
-
-    return this.http.get<Toy[]>(this.dinoUrl);
+  getToys(): Observable<Toy[]> {
+    return this.http.get<Toy[]>(this.toyUrl);
   }
 
-  getUserToys(id: number): Observable<Toy[]>{
-
+  getUserToys(id: number): Observable<Toy[]> {
     return this.http.get<Toy[]>(`${this.usersUrl}/toys/${id}`);
   }
 
-  getToyDetail(id: number): Observable<Toy>{
-
-    return this.http.get<Toy>(`${this.dinoUrl}/${id}`);
+  getToyDetail(id: number): Observable<Toy> {
+    return this.http.get<Toy>(`${this.toyUrl}/${id}`);
   }
 
-  updateToyAvailability(id: number): Observable<Toy>{
-
-    return this.http.put<Toy>(`${this.dinoUrl}/${id}`, {});
+  updateToyAvailability(id: number): Observable<Toy> {
+    return this.http.put<Toy>(`${this.toyUrl}/${id}`, {});
   }
-
 
   public saveToy(toy: Toy, id: number) {
     return this.http.post<Toy>(`${this.usersUrl}/${id}/toys`, toy);
   }
 
-  removeToy(id: number){
-    console.log(id, "transaction")
-    return this.http.delete<Toy>(`http://localhost:8080/toys/${id}`);
+  removeToy(id: number) {
+    console.log(id, 'transaction');
+    return this.http.delete<Toy>(`${this.toyUrl}/${id}`);
   }
 }
