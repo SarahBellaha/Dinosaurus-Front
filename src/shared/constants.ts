033ae4fbd78f1af309885cb283f2f4ba905fdbbb
loicWCS <loic.lebon.wcs@gmail.com>
2025-07-22 16:19:30 +0200

    add constants

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,3 +1,54 @@
-export const  TEXT = {
-  footer : "Dinosaurus - 2022",
-} as const
+export const TEXT = {
+  title: 'Bienvenue chez Dinosaurus',
+  footer: 'Dinosaurus - 2022',
+  home: 'Accueil',
+  toys: 'Jouets',
+  customerSpace: 'Espace client',
+  loggout: 'Déconnexion',
+  login: 'Se connecter',
+  register: "S'inscrire",
+  reserveToy: 'Réserver ce jouet',
+  reservedToys: 'Jouet réservé',
+  searchBar: 'Barre de recherche',
+  hello: 'Bonjour',
+} as const;
+
+export const ERRORS = {
+  login: 'Identifiant ou mot de passe incorrect',
+  mailRequired: "L'adresse e-mail est requise",
+  passwordRequired: 'Le mot de passe est requis',
+  invalidEmail: 'Adresse e-mail invalide',
+  firstnameRequired: 'Le prénom est requis',
+  lastnameRequired: 'Le nom est requis',
+  cityRequired: 'La ville est requise',
+  descriptionRequired: 'La description est requise',
+  pictureRequired: 'Le lien vers la photo est requis',
+
+} as const;
+
+export const REGISTER = {
+  title: 'Inscription',
+  firstName: 'Prénom',
+  lastName: 'Nom',
+  city: 'Ville',
+  mail: 'E-mail',
+  password: 'Mot de passe',
+  submit: 'Valider',
+} as const;
+
+export const ANNOUNCEMENT = {
+  title: 'Annonce',
+  description: 'Description',
+  pictureLink:"Lien vers la photo de l'objet",
+  submit: 'Poster !',
+  finalizeTrade: "Vos annonces ont trouvé preneur ! Acceptez les pour finaliser l'échange :)",
+  validateTrade: 'Valider l\'échange',
+  link: 'Lien de l\'annonce',
+  myAnnouncements: 'Mes annonces',
+  cancelAnnouncement: 'Annuler l\'annonce',
+  postAnnouncement: 'Poster une annonce',
+  informations: 'Informations',
+  currentRequests: 'Demandes en cours',
+  currentReservations: 'Réservations en cours',
+} as const;
+
